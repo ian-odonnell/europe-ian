8cb370f25b14447e91fcc9fef33782524f722f0e
Ian O'Donnell <admin@ian-donnell.com>
2017-12-06 16:32:59 +0000

    Database access possibly working?

--- client/api/chieveChatApi.js
+++ client/api/chieveChatApi.js
@@ -1,7 +1,7 @@
 import { ExternalApi } from './externalApi';
 // import getBaseUrl from '../baseUrl';
 
-var url = "http://localhost:3000/"; //getBaseUrl("chievechat");
+var url = "https://fullstack-seed.azurewebsites.net/"; //getBaseUrl("chievechat");
 var chieveChatApi = new ExternalApi(url);
 
 chieveChatApi.getChat = async() => {
